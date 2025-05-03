7731560bf01eb9b6d201d61b9818122201843c26
@ahmedoumerhouch <ahmed-oumerhouch@hotmail.com>
2025-05-02 21:34:33 -0400

    w

---
+++ js/swiperSlideNavigation.js
@@ -0,0 +1,17 @@
+﻿const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 69000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    noSwiping: true,                    // active le mode no-swiping
+    noSwipingClass: 'swiper-no-swiping' // la classe à ignorer
+});
